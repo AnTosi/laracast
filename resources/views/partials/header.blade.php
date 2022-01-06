8e8de04ec564c2cf0befdc2b79339e78d70fe484
AnTosi <andrea.tosi.uni@gmail.com>
2022-01-06 18:27:32 +0100

    created folder and header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,26 @@
+<header>
+    <div class="logo">
+        LARACASTS
+    </div>
+    <div class="center_nav">
+        <ul>
+            <li><a href="">TOPICS</a></li>
+            <li><a href="">SERIES</a></li>
+            <li><a href="">LARABITS <span>New!</span></a></li>
+            <li><a href="">DISCUSSIONS</a></li>
+            <li><a href="">PODCAST</a></li>
+        </ul>
+    </div>
+
+    <div class="search_area">
+        <div class="lens">
+            <i class="fas fa-search"></i>
+        </div>
+        <div>
+            SIGN IN
+        </div>
+        <div>
+            <button>GET STARTED</button>
+        </div>
+    </div>
+</header>
